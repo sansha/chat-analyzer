061dbe9edeac1e7259882db1bec531663deb5ddc
Sandesh <sandesh.sharma@tum.de>
2017-11-17 23:31:19 +0100

    implement conversion from whatsapp txt to json

--- convert_raw_data/txtConverter.js
+++ convert_raw_data/txtConverter.js
@@ -2,4 +2,22 @@
 var path = require('path');
 var fs = require('fs');
 // read profiles and led information
-var profiles = fs.readFileSync(path.join(__dirname, '/txt/test.txt'));
+var inputTxt = fs.readFileSync(path.join(__dirname, '/txt/test.txt'), 'utf8');
+var messagesRaw = inputTxt.split(/\n(?=\d\d\.\d\d\.\d\d, \d\d:\d\d - )/);
+
+let messagesParsed = messagesRaw.map(m => {
+    let idx = m.indexOf(" - ");
+    let msgAll = m.substr(idx + 3);
+    let totalDate = m.substr(0, idx);
+    let msgIdx = msgAll.indexOf(": ");
+    let author = msgAll.substr(0, msgIdx);
+    let msg = msgAll.substr(msgIdx + 2);
+
+    let [date, time] = totalDate.split(", ");
+    let [day, month, year] = date.split(".").map(a => parseInt(a));
+    let [hour, minute] = time.split(":").map(a => parseInt(a));
+    let timestamp = new Date(year+2000, month-1, day, hour, minute, 0, 0);
+     return {timestamp: +timestamp, author: author, msg: msg};
+});
+
+console.log(JSON.stringify(messagesParsed, null, 4));
